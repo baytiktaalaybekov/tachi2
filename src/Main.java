771396fd6300e10115cad5b79e5ffa37b1404dc2
baytiktaalaybekov <baytiktaalaybekov@gmail.com>
2022-12-05 16:09:23 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,133 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Java 8 үчүн практикалык тапшырма
+//
+//        1. Жаңы массив тизмесин түзүү үчүн Java программасын жазыңыз,
+//        бир нече шаарларды  кошуп, аларды консольго чыгарыныз.
+
+//        ArrayList<String>strings=new ArrayList<>();
+//        strings.add("Bishkek");
+//        strings.add("Kochkor");
+//        strings.add("Osh");
+//        strings.add("At-Bashyi");
+//        strings.add("Karakol");
+//        System.out.println(strings);
+        
+
+//        2. Массив тизмесине элементти биринчи орунга киргизүү үчүн Java программасын жаз.
+//        ArrayList<String>strings=new ArrayList<>();
+//        strings.add("Baytik");
+//        strings.add("Baytik");
+//        strings.add("Baytik");
+//        strings.add(0,"Taalaybekov");
+//        System.out.println(strings);
+
+//        3. Берилген массивдердин тизмесинен элементти (белгиленген индексте) алуу үчүн Java программасын жазыңыз.
+//        ArrayList<Integer>numbers=new ArrayList<>();
+//        numbers.add(2);
+//        numbers.add(1);
+//        numbers.add(5);
+//        numbers.add(10);
+//        System.out.println(numbers.get(2));
+//        System.out.println(numbers);
+//
+//        4. Берилген элемент боюнча белгилүү бир массив элементин озгортуу үчүн Java программасын жазыңыз.
+//        ArrayList<String>strings=new ArrayList<>();
+//      strings.add("b");
+//      strings.add("t");
+//      strings.add("a");
+//      strings.add("r");
+//      strings.set(2,"o");
+//      System.out.println(strings);
+
+//    5. Массив тизмесинен үчүнчү элементти очуруп салуу үчүн Java программасын жазыңыз.
+//        ArrayList<String>strings=new ArrayList<>();
+//      strings.add("b");
+//      strings.add("t");
+//      strings.add("a");
+//      strings.add("r");
+//      strings.add("y");
+//      strings.add("i");
+//      strings.remove(3);
+//        System.out.println(strings);
+//
+//        6. Массив тизмесиндеги элементти издөө үчүн Java программасын жазыңыз.
+//        ArrayList<Integer>numbers=new ArrayList<>();
+//        numbers.add(2);
+//        numbers.add(1);
+//        numbers.add(5);
+//        numbers.add(10);
+//        System.out.println(numbers.contains(2));
+
+//
+//        7. Берилген массивдердин тизмесин сорттоо үчүн Java программасын жазыңыз.
+//        ArrayList<Integer>numbers=new ArrayList<>();
+//        numbers.add(2);
+//        numbers.add(1);
+//        numbers.add(5);
+//        numbers.add(10);
+//        Collections.sort(numbers);
+//        System.out.println(numbers);
+
+//
+//        8. Бир массив тизмесин экинчи масивге көчүрүү үчүн Java программасын жазыңыз.
+//        ArrayList<Integer>numbers=new ArrayList<>();
+//        numbers.add(1);
+//        numbers.add(2);
+//        numbers.add(3);
+//        numbers.add(4);
+//        numbers.add(5);
+//        ArrayList<Integer>numbers1=new ArrayList<>();
+//        numbers1.add(1);
+//        numbers1.add(2);
+//        numbers1.add(3);
+//        numbers1.add(4);
+//        numbers1.add(5);
+//        System.out.println(numbers);
+//        System.out.println(numbers1);
+//        numbers.addAll(numbers1);
+//        System.out.println(numbers);
+
+//
+//        9. Массив тизмесиндеги элементтерди аралаштыруу үчүн Java программасын жазып.
+//        Маанилерин консольго чыгарыныз.
+//        ArrayList<Integer>numbers=new ArrayList<>();
+//        numbers.add(1);
+//        numbers.add(2);
+//        numbers.add(3);
+//        numbers.add(4);
+//        numbers.add(5);
+//        numbers.add(6);
+//        Collections.shuffle(numbers);
+//        System.out.println(numbers);
+
+//
+//        10. Массив тизмесиндеги элементтерди тескери өзгөртүү үчүн Java программасын жазыңыз.
+//        ArrayList<Integer>numbers=new ArrayList<>();
+//        numbers.add(1);
+//        numbers.add(2);
+//        numbers.add(3);
+//        numbers.add(4);
+//        numbers.add(5);
+//        Collections.reverse(numbers);
+//        System.out.println(numbers);
+//
+//        11. Аррейлисттин бир бөлүгүн алуу үчүн Java программасын жазыңыз
+//        ArrayList<Integer>numbers=new ArrayList<>();
+//        numbers.add(1);
+//        numbers.add(2);
+//        numbers.add(3);
+//        numbers.add(4);
+//        numbers.add(5);
+//        numbers.add(6);
+//        System.out.println(numbers.subList(0, 3));
+//        System.out.println(numbers);
+
+
+
+    }
+}
